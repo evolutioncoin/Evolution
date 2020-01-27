f3d1e3841a3e4b4e81867306601c867fba6735a0
“bytrixcoin” <bytrixcoin@protonmail.com>
2020-01-28 01:30:27 +0200

    daemon file cleanup

--- build/external/cryptopp/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/experimental/evolution-coin/Evolution-V2/Evolution-V2/external/cryptopp
-# Build directory: /home/experimental/evolution-coin/Evolution-V2/Evolution-V2/build/external/cryptopp
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
